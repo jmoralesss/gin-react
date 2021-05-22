97cf1a48e7a3dcd2fd7eff0c7e81b041c7875f6e
jmoralesss <mail@jmorales.net>
2021-05-22 01:45:56 -0500

    removed unnecessary import

--- client/src/Services/Member.service.ts
+++ client/src/Services/Member.service.ts
@@ -1,7 +1,6 @@
 import { AxiosResponse } from 'axios';
 import { Endpoint} from './Endpoint.enum'
 import { BaseInstance } from './Base.service'
-import { Member } from '../Types/Member.type';
 import { Response } from '../Services/Base.service'
 
 const responseBody = (response: AxiosResponse) => response.data;
